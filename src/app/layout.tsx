e94fd4d46cf6243323ff38f82fb110844783d65d
JorgeRico <jorge.rico.garriga@gmail.com>
2026-03-04 02:43:47 +0100

    feature: RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-// import { NextIntlClientProvider } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import "./styles/styles.css";
+import { getLocale } from 'next-intl/server';
 
-export default async function LocaleLayout({children}: LayoutProps<'/'>) {
+export default async function RootLayout({children}: LayoutProps<'/'>) {
+    const locale = await getLocale();
 
     return (
-        <html lang="en">
-            <body>
-                <Header></Header>
-                <main className="overflowHidden container">
-                    <section className="left w100 overflowHidden">
-                        {children}
-                    </section>
-                </main>
-                <Footer></Footer>
-            </body>
-        </html>
+        <NextIntlClientProvider>
+            <html lang={locale}>
+                <body>
+                    <Header></Header>
+                    <main className="overflowHidden container">
+                        <section className="left w100 overflowHidden">
+                            {children}
+                        </section>
+                    </main>
+                    <Footer></Footer>
+                </body>
+            </html>
+        </NextIntlClientProvider>
     );
 }
