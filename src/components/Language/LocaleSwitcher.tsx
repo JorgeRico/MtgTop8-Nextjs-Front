bedb1d12a69046b3d61169f374fd865d5df7723b
JorgeRico <jorge.rico.garriga@gmail.com>
2026-03-04 03:41:21 +0100

    feature: language

--- src/components/Language/LocaleSwitcher.tsx
+++ src/components/Language/LocaleSwitcher.tsx
@@ -1,35 +1,25 @@
 'use client';
 
 import {Locale, useLocale} from 'next-intl';
-// import {Locale, NextIntlClientProvider} from 'next-intl';
-
 
 type Props = {
-    changeLocaleAction: (locale: Locale) => Promise<void>;
+  changeLocaleAction: (locale: Locale) => Promise<void>;
 };
 
-// import {cookies} from 'next/headers';
-
 export default function LocaleSwitcher({changeLocaleAction}: Props) {
-    const locale = await getLocale();
+  const locale = useLocale();
 
-    // async function changeLocaleAction(locale: Locale) {
-    //     'use server';
-    //     const store = await cookies();
-    //     store.set('locale', locale);
-    // }
-
-    return (
-        <div style={{display: 'flex', gap: 5}}>
-        {['en', 'es', 'cat'].map((cur) => (
-            <button
-            key={cur}
-            onClick={() => changeLocaleAction(cur as Locale)}
-            style={{fontWeight: locale === cur ? 'bold' : 'normal'}}
-            >
-            {cur.toUpperCase()}
-            </button>
-        ))}
-        </div>
-    );
+  return (
+    <div style={{display: 'flex', gap: 5}}>
+      {['en', 'es', 'cat'].map((cur) => (
+        <button
+          key={cur}
+          onClick={() => changeLocaleAction(cur as Locale)}
+          style={{fontWeight: locale === cur ? 'bold' : 'normal'}}
+        >
+          {cur.toUpperCase()}
+        </button>
+      ))}
+    </div>
+  );
 }
