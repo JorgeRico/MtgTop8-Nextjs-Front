6824c5ebcb39f0525ab0ba3b266fb041eacee0d2
JorgeRico <jorge.rico.garriga@gmail.com>
2026-03-11 23:18:28 +0100

    hotfix: viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import type { Viewport, Metadata } from 'next';
 export const viewport: Viewport = {
 	width        : 'device-width',
 	initialScale : 1,
-	maximumScale : 1,
+	maximumScale : 5,
 	userScalable : false,
 }
 
