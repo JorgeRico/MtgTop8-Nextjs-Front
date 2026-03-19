69e06e1999f3a798e804644cf61c86429f569b5d
JorgeRico <jorge.rico.garriga@gmail.com>
2026-03-19 04:01:29 +0100

    hotfix: user scalable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,7 @@ import { baseUrl } from "@/types/baseUrl";
 export const viewport: Viewport = {
 	width        : 'device-width',
 	initialScale : 1,
-	maximumScale : 5,
-	userScalable : false,
+	maximumScale : 5
 }
 
 export const metadata: Metadata = {
