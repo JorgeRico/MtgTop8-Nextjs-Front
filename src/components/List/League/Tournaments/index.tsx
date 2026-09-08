1cc8cf5db44f3eee69dbbc893503d04e585c913d
JORGE RICO <jorgerico@MacBook-Air-de-JORGE.local>
2026-09-09 00:59:54 +0200

    hotfix: empty league

--- src/components/List/League/Tournaments/index.tsx
+++ src/components/List/League/Tournaments/index.tsx
@@ -51,7 +51,7 @@ const LeagueTournament: React.FC<LeagueIdType> = ({ id }) => {
                                 <Pagination text={t('tournament.Tournaments')} total={total} itemsPerPage={total} currentPage={currentPage} setCurrentPage={setCurrentPage}></Pagination>
                             </>
                         ) : (
-                                <div className="left w100 mt40 mb40">
+                                <div className="left w100 mt20 mb40">
                                     <div className="left w100 f14">{t('tournament.NoTournaments')}</div>
                                 </div>
                         )}
